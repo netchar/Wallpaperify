eea70e2fbcdb95b6949a6539a9ab2aed506ffded
Eugene Glushankov <e.glushankov@gmail.com>
2019-05-09 21:53:58 +0300

    Collections page
     * Extracted basic inner viewModel for lists

--- app/src/main/java/com/netchar/wallpaperify/ui/collections/CollectionsViewModel.kt
+++ app/src/main/java/com/netchar/wallpaperify/ui/collections/CollectionsViewModel.kt
@@ -5,36 +5,21 @@ import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations
 import com.netchar.common.base.BaseViewModel
 import com.netchar.common.utils.CoroutineDispatchers
-import com.netchar.common.utils.SingleLiveData
 import com.netchar.models.Collection
 import com.netchar.models.apirequest.ApiRequest
-import com.netchar.models.apirequest.Paging
 import com.netchar.models.uimodel.ErrorMessage
 import com.netchar.models.uimodel.Message
-import com.netchar.remote.Resource
-import com.netchar.remote.enums.Cause
 import com.netchar.repository.collection.ICollectionRepository
-import com.netchar.wallpaperify.R
+import com.netchar.wallpaperify.ui.base.BasicEndlessListViewModel
 import javax.inject.Inject
 
-
-/**
- * Created by Netchar on 08.05.2019.
- * e.glushankov@gmail.com
- */
 
 class CollectionsViewModel @Inject constructor(
         dispatchers: CoroutineDispatchers,
         private val repository: ICollectionRepository
 ) : BaseViewModel(dispatchers) {
-    private val paging = Paging(startPage = 1)
     private val request = MediatorLiveData<ApiRequest.Collections>()
-
-    private val _errorPlaceholder = SingleLiveData<ErrorMessage>()
-    private val _toast = SingleLiveData<Message>()
-    private val _error = SingleLiveData<ErrorMessage>()
-    private val _refreshing = SingleLiveData<Boolean>()
-    private val _collections: MediatorLiveData<List<Collection>> = MediatorLiveData()
+    private val listViewModel = BasicEndlessListViewModel<Collection>()
 
     private val repositoryLiveData = Transformations.switchMap(request) { request ->
         repository.getCollections(request, scope).getLiveData()
@@ -42,97 +27,30 @@ class CollectionsViewModel @Inject constructor(
 
     init {
         addMediatorSources()
-        requestPhotos(paging.fromStart())
+        requestPhotos(listViewModel.paging.fromStart())
     }
 
-    val collections: LiveData<List<Collection>> get() = _collections
-    val refreshing: LiveData<Boolean> get() = _refreshing
-    val error: LiveData<ErrorMessage> get() = _error
-    val toast: LiveData<Message> get() = _toast
-    val errorPlaceholder: LiveData<ErrorMessage> get() = _errorPlaceholder
+    val collections: LiveData<List<Collection>> get() = listViewModel.items
+    val refreshing: LiveData<Boolean> get() = listViewModel.refreshing
+    val error: LiveData<ErrorMessage> get() = listViewModel.error
+    val toast: LiveData<Message> get() = listViewModel.toast
+    val errorPlaceholder: LiveData<ErrorMessage> get() = listViewModel.errorPlaceholder
 
     fun refresh() {
-        requestPhotos(paging.fromStart())
+        requestPhotos(listViewModel.paging.fromStart())
     }
 
     fun loadMore() {
-        requestPhotos(paging.nextPage())
+        requestPhotos(listViewModel.paging.nextPage())
     }
 
     private fun addMediatorSources() {
-        _collections.addSource(repositoryLiveData) { response ->
-            proceedFetching(response)
+        listViewModel.items.addSource(repositoryLiveData) { response ->
+            listViewModel.proceedFetching(response)
         }
     }
 
     private fun requestPhotos(page: Int) {
         request.value = ApiRequest.Collections(page)
-    }
-
-    private fun proceedFetching(response: Resource<List<Collection>>) {
-        if (needToHidePlaceholder) {
-            hidePlaceholderError()
-        }
-
-        if (isFreshFetching) {
-            handleFreshFetch(response)
-        } else {
-            handleLoadMoreFetch(response)
-        }
-    }
-
-    private val needToHidePlaceholder get() = isNoItemsVisible && _errorPlaceholder.value?.isVisible == true
-
-    private val isFreshFetching get() = paging.currentPage == paging.startPage
-
-    private fun handleFreshFetch(response: Resource<List<Collection>>) {
-        when (response) {
-            is Resource.Success -> {
-                _collections.value = response.data
-                _toast.value = Message(R.string.latest_message_data_updated)
-            }
-            is Resource.Loading -> {
-                _refreshing.value = response.isLoading
-            }
-            is Resource.Error -> {
-                _refreshing.value = false
-                riseError(response)
-            }
-        }
-    }
-
-    private fun handleLoadMoreFetch(response: Resource<List<Collection>>) {
-        when (response) {
-            is Resource.Success -> {
-                _collections.apply { value = value?.plus(response.data) }
-            }
-            is Resource.Error -> {
-                paging.prevPage()
-                riseError(response)
-            }
-        }
-    }
-
-    private fun hidePlaceholderError() {
-        _errorPlaceholder.value = ErrorMessage.empty()
-    }
-
-    private val isNoItemsVisible get() = _collections.value.isNullOrEmpty()
-
-    private fun riseError(response: Resource.Error) {
-        val errorMessage = getErrorMessage(response)
-
-        if (isNoItemsVisible) {
-            _errorPlaceholder.value = errorMessage
-        } else {
-            _error.value = errorMessage
-        }
-    }
-
-    private fun getErrorMessage(response: Resource.Error): ErrorMessage {
-        return when (response.cause) {
-            Cause.NO_INTERNET_CONNECTION -> ErrorMessage(true, Message(R.string.error_message_no_internet), R.drawable.ic_no_internet_connection)
-            Cause.NOT_AUTHENTICATED, Cause.UNEXPECTED -> ErrorMessage(true, Message(R.string.error_message_try_again_later), R.drawable.img_unexpected_error)
-        }
     }
 }
--- app/src/main/java/com/netchar/wallpaperify/ui/photos/PhotosViewModel.kt
+++ app/src/main/java/com/netchar/wallpaperify/ui/photos/PhotosViewModel.kt
@@ -6,16 +6,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.netchar.common.base.BaseViewModel
 import com.netchar.common.utils.CoroutineDispatchers
-import com.netchar.common.utils.SingleLiveData
 import com.netchar.models.Photo
 import com.netchar.models.apirequest.ApiRequest
-import com.netchar.models.apirequest.Paging
 import com.netchar.models.uimodel.ErrorMessage
 import com.netchar.models.uimodel.Message
-import com.netchar.remote.Resource
-import com.netchar.remote.enums.Cause
 import com.netchar.repository.photos.IPhotosRepository
-import com.netchar.wallpaperify.R
+import com.netchar.wallpaperify.ui.base.BasicEndlessListViewModel
 import javax.inject.Inject
 
 /**
@@ -28,15 +24,9 @@ class PhotosViewModel @Inject constructor(
         dispatchers: CoroutineDispatchers
 ) : BaseViewModel(dispatchers) {
 
-    private val paging = Paging(startPage = 1)
     private val request = MediatorLiveData<ApiRequest.Photos>()
-
-    private val _errorPlaceholder = SingleLiveData<ErrorMessage>()
-    private val _toast = SingleLiveData<Message>()
-    private val _error = SingleLiveData<ErrorMessage>()
-    private val _refreshing = SingleLiveData<Boolean>()
+    private val listViewModel = BasicEndlessListViewModel<Photo>()
     private val _ordering = MutableLiveData<ApiRequest.Order>()
-    private val _photos: MediatorLiveData<List<Photo>> = MediatorLiveData()
 
     private val repositoryLiveData = Transformations.switchMap(request) { request ->
         repository.getPhotos(request, scope).getLiveData()
@@ -44,22 +34,22 @@ class PhotosViewModel @Inject constructor(
 
     init {
         addMediatorSources()
-        requestPhotos(paging.fromStart(), ApiRequest.Order.LATEST)
+        requestPhotos(listViewModel.paging.fromStart(), ApiRequest.Order.LATEST)
     }
 
-    val photos: LiveData<List<Photo>> get() = _photos
-    val refreshing: LiveData<Boolean> get() = _refreshing
-    val error: LiveData<ErrorMessage> get() = _error
-    val toast: LiveData<Message> get() = _toast
-    val errorPlaceholder: LiveData<ErrorMessage> get() = _errorPlaceholder
+    val photos: LiveData<List<Photo>> get() = listViewModel.items
+    val refreshing: LiveData<Boolean> get() = listViewModel.refreshing
+    val error: LiveData<ErrorMessage> get() = listViewModel.error
+    val toast: LiveData<Message> get() = listViewModel.toast
+    val errorPlaceholder: LiveData<ErrorMessage> get() = listViewModel.errorPlaceholder
     val ordering: LiveData<ApiRequest.Order> get() = _ordering
 
     fun refresh() {
-        requestPhotos(paging.fromStart(), getOrderingOrDefault())
+        requestPhotos(listViewModel.paging.fromStart(), getOrderingOrDefault())
     }
 
     fun loadMore() {
-        requestPhotos(paging.nextPage(), getOrderingOrDefault())
+        requestPhotos(listViewModel.paging.nextPage(), getOrderingOrDefault())
     }
 
     fun orderBy(ordering: ApiRequest.Order) {
@@ -68,83 +58,16 @@ class PhotosViewModel @Inject constructor(
 
     private fun addMediatorSources() {
         request.addSource(_ordering) { ordering ->
-            requestPhotos(paging.fromStart(), ordering)
+            requestPhotos(listViewModel.paging.fromStart(), ordering)
         }
 
-        _photos.addSource(repositoryLiveData) { response ->
-            proceedFetching(response)
+        listViewModel.items.addSource(repositoryLiveData) { response ->
+            listViewModel.proceedFetching(response)
         }
     }
 
     private fun requestPhotos(page: Int, order: ApiRequest.Order) {
         request.value = ApiRequest.Photos(page, order)
-    }
-
-    private fun proceedFetching(response: Resource<List<Photo>>) {
-        if (needToHidePlaceholder) {
-            hidePlaceholderError()
-        }
-
-        if (isFreshFetching) {
-            handleFreshFetch(response)
-        } else {
-            handleLoadMoreFetch(response)
-        }
-    }
-
-    private val needToHidePlaceholder get() = isNoItemsVisible && _errorPlaceholder.value?.isVisible == true
-
-    private val isFreshFetching get() = paging.currentPage == paging.startPage
-
-    private fun handleFreshFetch(response: Resource<List<Photo>>) {
-        when (response) {
-            is Resource.Success -> {
-                _photos.value = response.data
-                _toast.value = Message(R.string.latest_message_data_updated)
-            }
-            is Resource.Loading -> {
-                _refreshing.value = response.isLoading
-            }
-            is Resource.Error -> {
-                _refreshing.value = false
-                riseError(response)
-            }
-        }
-    }
-
-    private fun handleLoadMoreFetch(response: Resource<List<Photo>>) {
-        when (response) {
-            is Resource.Success -> {
-                _photos.apply { value = value?.plus(response.data) }
-            }
-            is Resource.Error -> {
-                paging.prevPage()
-                riseError(response)
-            }
-        }
-    }
-
-    private fun hidePlaceholderError() {
-        _errorPlaceholder.value = ErrorMessage.empty()
-    }
-
-    private val isNoItemsVisible get() = _photos.value.isNullOrEmpty()
-
-    private fun riseError(response: Resource.Error) {
-        val errorMessage = getErrorMessage(response)
-
-        if (isNoItemsVisible) {
-            _errorPlaceholder.value = errorMessage
-        } else {
-            _error.value = errorMessage
-        }
-    }
-
-    private fun getErrorMessage(response: Resource.Error): ErrorMessage {
-        return when (response.cause) {
-            Cause.NO_INTERNET_CONNECTION -> ErrorMessage(true, Message(R.string.error_message_no_internet), R.drawable.ic_no_internet_connection)
-            Cause.NOT_AUTHENTICATED, Cause.UNEXPECTED -> ErrorMessage(true, Message(R.string.error_message_try_again_later), R.drawable.img_unexpected_error)
-        }
     }
 
     private fun getOrderingOrDefault(): ApiRequest.Order = _ordering.value ?: ApiRequest.Order.LATEST
