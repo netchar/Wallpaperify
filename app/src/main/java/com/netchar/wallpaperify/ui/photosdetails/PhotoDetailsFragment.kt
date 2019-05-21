8ccfc985e15e4336940d59933caf4a093b5f5274
Eugen Glushankov <eugenglushankov@coherentsolutions.com>
2019-05-21 18:29:52 +0300

    Photos details page
     * Added initial animations.

--- app/src/main/java/com/netchar/wallpaperify/ui/photosdetails/PhotoDetailsFragment.kt
+++ app/src/main/java/com/netchar/wallpaperify/ui/photosdetails/PhotoDetailsFragment.kt
@@ -3,6 +3,9 @@ package com.netchar.wallpaperify.ui.photosdetails
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 import androidx.transition.*
 import com.bumptech.glide.Glide
@@ -13,6 +16,7 @@ import com.bumptech.glide.request.target.Target
 import com.netchar.common.base.BaseFragment
 import com.netchar.common.extensions.toVisible
 import com.netchar.wallpaperify.R
+import com.transitionseverywhere.extra.Scale
 import kotlinx.android.synthetic.main.fragment_photo_details.*
 
 class PhotoDetailsFragment : BaseFragment() {
@@ -21,79 +25,121 @@ class PhotoDetailsFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val transition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+//        val transition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+//        transition.duration = 2000
+//        transition.interpolator = BounceInterpolator()
+        val transition = TransitionSet()
+        transition.duration = 250
+        transition.addTransition(ChangeBounds())
+        transition.addTransition(ChangeTransform())
+        transition.addTransition(ChangeClipBounds())
+        transition.addTransition(ChangeImageTransform())
+
         sharedElementEnterTransition = transition
+//        val enterTrans = Fade()
+//        enterTrans.duration = 1000
+//        enterTrans.interpolator = AccelerateDecelerateInterpolator()
+//        enterTransition = enterTrans
+
 //        exitTransition= TransitionInflater.from(context).inflateTransition(android.R.transition.explode)
         transition.addListener(object : TransitionListenerAdapter() {
 
             override fun onTransitionEnd(transition: Transition) {
 
                 toolbar?.let {
-                    val toolbarSet = TransitionSet().apply {
-                        addTransition(Fade())
-                        addTarget(it)
-                        addTarget(photo_details_floating_action_btn)
+                    //                    val toolbarSet = TransitionSet().apply {
+//                        addTransition(Fade())
+//                        addTarget(it)
 //                        addTarget(photo_details_bottom_sheet_layout)
-                    }
-                    TransitionManager.beginDelayedTransition(photo_details_coordinator, toolbarSet)
-                    it.toVisible()
-                    photo_details_floating_action_btn.toVisible()
+//                    }
+//                    TransitionManager.beginDelayedTransition(photo_details_coordinator, toolbarSet)
+//                    it.toVisible()
 //                    photo_details_bottom_sheet_layout.toVisible()
-                }
 
-//
-//                val set = TransitionSet().apply {
-//                    addTransition(Scale(0.3f))
-//                    addTransition(Fade())
-//                    addTarget(photo_details_bottom_sheet)
-//                }
-//                TransitionManager.beginDelayedTransition(photo_details_bottom_sheet, set)
-//                photo_details_bottom_sheet.toVisible()
+                    val set = TransitionSet()
+
+                    val first = Scale(0.3f)
+                    first.addTarget(photo_details_floating_action_btn)
+
+                    val second = Fade()
+                    second.addTarget(it)
+
+
+                    val third = ChangeBounds()
+                    third.addTarget(photo_details_bottom_sheet_layout)
+
+                    set.setOrdering(TransitionSet.ORDERING_TOGETHER)
+                            .addTransition(first)
+                            .addTransition(second)
+                            .addTransition(third)
+
+                    TransitionManager.beginDelayedTransition(photo_details_coordinator, set)
+
+                    photo_details_floating_action_btn.toVisible()
+                    it.toVisible()
+                    photo_details_bottom_sheet_layout.translationY = 0f
+
+//                    val ss = ConstraintSet()
+//                    ss.setTranslationY(photo_details_bottom_sheet_layout.id, -400f)
+//                    ss.applyTo(photo_details_bottom_sheet_layout)
+                }
             }
         })
     }
 
-//    inline fun <reified T : ViewGroup.LayoutParams> View.layoutParams(block: T.() -> Unit) {
-//        if (layoutParams is T) block(layoutParams as T)
-//    }
-//
-//    fun View.margin(left: Float? = null, top: Float? = null, right: Float? = null, bottom: Float? = null) {
-//        layoutParams<ViewGroup.MarginLayoutParams> {
-//            left?.run { leftMargin = this) }
-//            top?.run { topMargin = dpToPx(this) }
-//            right?.run { rightMargin = dpToPx(this) }
-//            bottom?.run { bottomMargin = dpToPx(this) }
-//        }
-//    }
-
-
     override fun onDestroyView() {
         super.onDestroyView()
-//        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        val flags = activity?.window?.decorView?.systemUiVisibility!!
-        activity?.window?.decorView?.systemUiVisibility = flags or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//        val flags = activity?.window?.decorView?.systemUiVisibility!!
+//        activity?.window?.decorView?.systemUiVisibility = flags or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 //        activity?.window?.navigationBarColor = getThemeAttrColor(context!!, android.R.attr.navigationBarColor)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//        view.setOnApplyWindowInsetsListener { view, insets ->
+//            var consumed = false
+//
+//            (view as ViewGroup).forEach { child ->
+//                // Dispatch the insets to the child
+//                val childResult = child.dispatchApplyWindowInsets(insets)
+//                // If the child consumed the insets, record it
+//                if (childResult.isConsumed) {
+//                    consumed = true
+//                }
+//            }
+//
+//            // If any of the children consumed the insets, return
+//            // an appropriate value
+//            if (consumed) insets.consumeSystemWindowInsets() else insets
+//        }
 
-        var flags = activity?.window?.decorView?.systemUiVisibility!!
-        flags = flags xor View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        activity?.window?.decorView?.systemUiVisibility = flags
+
+//        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//        photo_details_coordinator.setOnApplyWindowInsetsListener { v, insets ->
+//            insets
+//        }
+//        photo_details_coordinator.requestApplyInsetsWhenAttached()
+
+//        var flags = activity?.window?.decorView?.systemUiVisibility!!
+//        flags = flags xor View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//        activity?.window?.decorView?.systemUiVisibility = flags
 
 //        activity?.window?.decorView?.systemUiVisibility  = flags
 //        activity?.window?.navigationBarColor = Color.BLACK
         photo_details_coordinator.setOnApplyWindowInsetsListener { _, windowInsets ->
             toolbar?.updatePadding(top = windowInsets.systemWindowInsetTop, bottom = 0)
-//            photo_details_bottom_sheet_layout.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-//                setMargins(0,0,0, windowInsets.systemWindowInsetBottom)
-//            }
+            photo_details_bottom_sheet_layout.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                setMargins(0, 0, 0, windowInsets.systemWindowInsetBottom)
+            }
 //            photo_details_floating_action_btn.updatePadding(bottom = 0)
             windowInsets.consumeSystemWindowInsets()
         }
+
+//
+//        photo_details_bottom_sheet.getConstraintSet(R.id.start)?.let { startConstraintSet ->
+//            startConstraintSet.constrainMinHeight(R.id.photo_details_bottom_sheet_layout, 500.dp)
+//        }
 
         arguments?.let {
             val safeArgs = PhotoDetailsFragmentArgs.fromBundle(it)
@@ -101,19 +147,22 @@ class PhotoDetailsFragment : BaseFragment() {
             photo_details_image.transitionName = safeArgs.imageTransitionName
 
             Glide.with(this)
-                .load(safeArgs.photoUrl)
-                .listener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                        startPostponedEnterTransition()
-                        return false
-                    }
+                    .load(safeArgs.photoUrl)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            startPostponedEnterTransition()
+                            return false
+                        }
 
-                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                        startPostponedEnterTransition()
-                        return false
-                    }
-                })
-                .into(photo_details_image)
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            startPostponedEnterTransition()
+                            return false
+                        }
+                    })
+                    .into(photo_details_image)
+
+
+//            view.dispatchApplyWindowInsets(activity!!.window.decorView.rootWindowInsets)
 
 //            val bottomSheetBehavior = BottomSheetBehavior.from(photo_details_bottom_sheet)
 //            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
--- common/src/main/java/com/netchar/common/extensions/ViewExt.kt
+++ common/src/main/java/com/netchar/common/extensions/ViewExt.kt
@@ -73,3 +73,21 @@ fun View.closeSoftKeyboard() {
     val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(this.windowToken, 0)
 }
+
+fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        // We're already attached, just request as normal
+        requestApplyInsets()
+    } else {
+        // We're not attached to the hierarchy, add a listener to
+        // request when we are
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.removeOnAttachStateChangeListener(this)
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
