71545af01ae2a04913bf6bf4b3e724168ce7dd7a
Eugene Glushankov <e.glushankov@gmail.com>
2019-05-22 23:29:54 +0300

    Collections page
     * Refactored

--- app/src/main/java/com/netchar/wallpaperify/ui/photosdetails/PhotoDetailsFragment.kt
+++ app/src/main/java/com/netchar/wallpaperify/ui/photosdetails/PhotoDetailsFragment.kt
@@ -25,6 +25,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.animation.LinearInterpolator
 import android.view.animation.OvershootInterpolator
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.updatePadding
 import androidx.transition.*
@@ -33,6 +34,7 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.netchar.common.base.BaseFragment
 import com.netchar.common.extensions.showToast
 import com.netchar.common.extensions.toGone
@@ -42,11 +44,12 @@ import com.netchar.wallpaperify.R
 import com.transitionseverywhere.extra.Scale
 import kotlinx.android.synthetic.main.fragment_photo_details.*
 
-
 class PhotoDetailsFragment : BaseFragment() {
 
     private var isMenuOpen: Boolean = false
-    var interpolator = OvershootInterpolator()
+    private var interpolator = OvershootInterpolator()
+    private val initialFabTranslationY = 100f
+    private val initialFabLabelTranslationX = 100f
 
     override val layoutResId: Int = R.layout.fragment_photo_details
 
@@ -76,90 +79,6 @@ class PhotoDetailsFragment : BaseFragment() {
         restoreStatusBarsThemeColors()
     }
 
-    private val initialFabTranslationY = 100f
-
-    fun initFabs() {
-        photo_details_floating_download.alpha = 0f
-        photo_details_floating_raw.alpha = 0f
-        photo_details_floating_wallpaper.alpha = 0f
-        textView3.alpha = 0f
-        textView4.alpha = 0f
-        textView5.alpha = 0f
-
-        textView3.translationX = initialFabTranslationY * 2
-        textView4.translationX = initialFabTranslationY * 2
-        textView5.translationX = initialFabTranslationY * 2
-
-        photo_details_floating_download.translationY = initialFabTranslationY
-        photo_details_floating_raw.translationY = initialFabTranslationY
-        photo_details_floating_wallpaper.translationY = initialFabTranslationY
-
-        photo_details_floating_download.setOnClickListener(fabClickListener)
-        photo_details_floating_raw.setOnClickListener(fabClickListener)
-        photo_details_floating_wallpaper.setOnClickListener(fabClickListener)
-
-        photo_details_floating_action_btn.setOnClickListener(fabClickListener)
-        fab_overlay.setOnClickListener(fabClickListener)
-
-        fab_overlay.toGone()
-    }
-
-    private fun openMenu() {
-        isMenuOpen = !isMenuOpen
-
-        photo_details_floating_action_btn.animate().setInterpolator(interpolator).rotation(180f).setDuration(300).start()
-
-        photo_details_floating_download.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
-        textView3.animate().setStartDelay(0).translationX(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
-
-        photo_details_floating_raw.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
-        textView4.animate().setStartDelay(50).translationX(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
-
-        photo_details_floating_wallpaper.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
-        textView5.animate().setStartDelay(100).translationX(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
-
-        TransitionManager.beginDelayedTransition(photo_details_main_constraint)
-        fab_overlay.toVisible()
-    }
-
-    private fun closeMenu() {
-        isMenuOpen = !isMenuOpen
-
-        photo_details_floating_action_btn.animate().setInterpolator(interpolator).rotation(0f).setDuration(300).start()
-
-        photo_details_floating_download.animate().translationY(initialFabTranslationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
-        textView3.animate().translationX(initialFabTranslationY * 2).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
-
-        photo_details_floating_raw.animate().translationY(initialFabTranslationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
-        textView4.animate().translationX(initialFabTranslationY * 2).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
-
-        photo_details_floating_wallpaper.animate().translationY(initialFabTranslationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
-        textView5.animate().translationX(initialFabTranslationY * 2).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
-
-        TransitionManager.beginDelayedTransition(photo_details_main_constraint)
-        fab_overlay.toGone()
-    }
-
-    private val fabClickListener = View.OnClickListener {
-        //        when (it.id) {
-//            R.id.photo_details_menu_item_wallpaper -> {
-//                showToast("Wallpaper")
-//            }
-//            R.id.photo_details_menu_item_raw -> {
-//                showToast("Raw")
-//            }
-//            R.id.photo_details_menu_item_download -> {
-//                showToast("Download")
-//            }
-//        }
-        if (isMenuOpen) {
-            closeMenu()
-        } else {
-            openMenu()
-        }
-    }
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -172,39 +91,20 @@ class PhotoDetailsFragment : BaseFragment() {
             postponeEnterTransition()
             val safeArgs = PhotoDetailsFragmentArgs.fromBundle(args)
             photo_details_image.transitionName = safeArgs.imageTransitionName
-
-//            photo_details_floating_action_btn.inflate(R.menu.menu_photo_details_fab)
-//            photo_details_floating_action_btn.setOnActionSelectedListener { actionItem ->
-//                when (actionItem.id) {
-//                    R.id.photo_details_menu_item_wallpaper -> {
-//                        showToast("Wallpaper")
-//                    }
-//                    R.id.photo_details_menu_item_raw -> {
-//                        showToast("Raw")
-//                    }
-//                    R.id.photo_details_menu_item_download -> {
-//                        showToast("Download")
-//                    }
-//                }
-//
-//                photo_details_floating_action_btn.close(true)
-//                true
-//            }
-
             Glide.with(this)
-                    .load(safeArgs.photoUrl)
-                    .listener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                            startPostponedEnterTransition()
-                            return false
-                        }
+                .load(safeArgs.photoUrl)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        startPostponedEnterTransition()
+                        return false
+                    }
 
-                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                            startPostponedEnterTransition()
-                            return false
-                        }
-                    })
-                    .into(photo_details_image)
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        startPostponedEnterTransition()
+                        return false
+                    }
+                })
+                .into(photo_details_image)
         }
     }
 
@@ -242,17 +142,16 @@ class PhotoDetailsFragment : BaseFragment() {
 
             fragmentToolbar?.let { toolbar ->
 
-                val set = getPreparedTransition(toolbar)
-
-                TransitionManager.beginDelayedTransition(photo_details_coordinator, set)
+                val transitionSet = getEnterTransitionSet(toolbar)
+                TransitionManager.beginDelayedTransition(photo_details_coordinator, transitionSet)
 
                 photo_details_bottom_panel_constraint.toVisible()
-                photo_details_floating_action_btn.toVisible()
+                photo_details_floating_main.toVisible()
                 toolbar.toVisible()
             }
         }
 
-        private fun getPreparedTransition(toolbar: Toolbar): TransitionSet {
+        private fun getEnterTransitionSet(toolbar: Toolbar): TransitionSet {
 
             val titleAndBottomPanelTransition = TransitionSet().apply {
                 addTransition(Fade())
@@ -265,13 +164,13 @@ class PhotoDetailsFragment : BaseFragment() {
             val fabButtonTransition = TransitionSet().apply {
                 addTransition(Scale(0.2f))
                 addTransition(Fade())
-                addTarget(photo_details_floating_action_btn)
+                addTarget(photo_details_floating_main)
             }
 
             return TransitionSet()
-                    .setOrdering(TransitionSet.ORDERING_SEQUENTIAL)
-                    .addTransition(titleAndBottomPanelTransition)
-                    .addTransition(fabButtonTransition)
+                .setOrdering(TransitionSet.ORDERING_SEQUENTIAL)
+                .addTransition(titleAndBottomPanelTransition)
+                .addTransition(fabButtonTransition)
         }
     }
 
@@ -295,5 +194,105 @@ class PhotoDetailsFragment : BaseFragment() {
             photo_details_bottom_panel_constraint.updatePadding(bottom = windowInsets.systemWindowInsetBottom)
             windowInsets.consumeSystemWindowInsets()
         }
+    }
+
+    private fun initFabs() {
+        photo_details_floating_download.alpha = 0f
+        photo_details_floating_raw.alpha = 0f
+        photo_details_floating_wallpaper.alpha = 0f
+        photo_details_floating_label_download.alpha = 0f
+        photo_details_floating_label_raw.alpha = 0f
+        photo_details_floating_label_wallpaper.alpha = 0f
+
+        photo_details_floating_label_download.translationX = initialFabLabelTranslationX
+        photo_details_floating_label_raw.translationX = initialFabLabelTranslationX
+        photo_details_floating_label_wallpaper.translationX = initialFabLabelTranslationX
+
+        photo_details_floating_download.translationY = initialFabTranslationY
+        photo_details_floating_raw.translationY = initialFabTranslationY
+        photo_details_floating_wallpaper.translationY = initialFabTranslationY
+
+        val fabClickListener = View.OnClickListener {
+
+            when (it.id) {
+                R.id.photo_details_floating_download,
+                R.id.photo_details_floating_label_download -> {
+                    showToast("Download")
+                }
+                R.id.photo_details_floating_raw,
+                R.id.photo_details_floating_label_raw -> {
+                    showToast("Raw")
+                }
+                R.id.photo_details_floating_wallpaper,
+                R.id.photo_details_floating_label_wallpaper -> {
+                    showToast("Wallpaper")
+                }
+            }
+
+            closeMenu()
+        }
+
+        photo_details_floating_download.setOnClickListener(fabClickListener)
+        photo_details_floating_raw.setOnClickListener(fabClickListener)
+        photo_details_floating_wallpaper.setOnClickListener(fabClickListener)
+
+        photo_details_floating_label_download.setOnClickListener(fabClickListener)
+        photo_details_floating_label_raw.setOnClickListener(fabClickListener)
+        photo_details_floating_label_wallpaper.setOnClickListener(fabClickListener)
+
+        photo_details_floating_main.setOnClickListener {
+            if (isMenuOpen) {
+                closeMenu()
+            } else {
+                openMenu()
+            }
+        }
+
+        fab_overlay.setOnClickListener { closeMenu() }
+        fab_overlay.toGone()
+    }
+
+    private fun openMenu() {
+        isMenuOpen = !isMenuOpen
+
+        animateRotateMainFab(180f)
+        animateFabMenuItemOpen(photo_details_floating_download, photo_details_floating_label_download)
+        animateFabMenuItemOpen(photo_details_floating_raw, photo_details_floating_label_raw)
+        animateFabMenuItemOpen(photo_details_floating_wallpaper, photo_details_floating_label_wallpaper)
+        animateOverlayShow()
+    }
+
+    private fun closeMenu() {
+        isMenuOpen = !isMenuOpen
+
+        animateRotateMainFab(0f)
+        animateFabMenuItemClose(photo_details_floating_download, photo_details_floating_label_download)
+        animateFabMenuItemClose(photo_details_floating_raw, photo_details_floating_label_raw)
+        animateFabMenuItemClose(photo_details_floating_wallpaper, photo_details_floating_label_wallpaper)
+        animateOverlayHide()
+    }
+
+    private fun animateOverlayShow() {
+        TransitionManager.beginDelayedTransition(photo_details_main_constraint)
+        fab_overlay.toVisible()
+    }
+
+    private fun animateRotateMainFab(angle: Float) {
+        photo_details_floating_main.animate().setInterpolator(interpolator).rotation(angle).setDuration(300).start()
+    }
+
+    private fun animateOverlayHide() {
+        TransitionManager.beginDelayedTransition(photo_details_main_constraint)
+        fab_overlay.toGone()
+    }
+
+    private fun animateFabMenuItemOpen(fab: FloatingActionButton, label: TextView) {
+        fab.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
+        label.animate().setStartDelay(0).translationX(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start()
+    }
+
+    private fun animateFabMenuItemClose(fab: FloatingActionButton, label: TextView) {
+        fab.animate().translationY(initialFabTranslationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
+        label.animate().translationX(initialFabLabelTranslationX).alpha(0f).setInterpolator(interpolator).setDuration(300).start()
     }
 }
