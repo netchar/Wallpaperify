f0e4fe033bfd6c75b8274634c3286e52c62417e6
Eugen Glushankov <eugenglushankov@coherentsolutions.com>
2019-06-06 19:40:00 +0300

    Themes
     * Added async diff callback using actor

--- common/src/main/java/com/netchar/common/poweradapter/adapter/RecyclerDataSource.kt
+++ common/src/main/java/com/netchar/common/poweradapter/adapter/RecyclerDataSource.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2019 Eugene Glushankov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.netchar.common.poweradapter.adapter
 
 import androidx.annotation.LayoutRes
@@ -7,24 +23,50 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.netchar.common.poweradapter.item.IRecyclerItem
 import com.netchar.common.poweradapter.item.ItemRenderer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.withContext
 import java.lang.ref.WeakReference
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by Netchar on 02.04.2019.
  * e.glushankov@gmail.com
  */
 
-open class RecyclerDataSource(private val renderers: List<ItemRenderer>) {
+open class RecyclerDataSource(private val renderers: List<ItemRenderer>) : CoroutineScope {
     protected val data = mutableListOf<IRecyclerItem>()
     protected lateinit var adapterReference: WeakReference<RecyclerView.Adapter<RecyclerViewHolder>>
         private set
 
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+    private val diffCallback by lazy(LazyThreadSafetyMode.NONE) {
+        RecyclerDiffCallback()
+    }
+
+    private val updateActor = actor<List<IRecyclerItem>>(capacity = CONFLATED) {
+        for (list in channel) internalUpdate(list)
+    }
+
     @MainThread
     open fun setData(newData: List<IRecyclerItem>) {
-        val diffResult = DiffUtil.calculateDiff(RecyclerDiffCallback(data, newData))
+        updateActor.offer(newData)
+    }
+
+    @MainThread
+    private suspend fun internalUpdate(newData: List<IRecyclerItem>) {
+        val result = withContext(Dispatchers.Default) {
+            DiffUtil.calculateDiff(diffCallback.apply { update(data, newData.toList()) })
+        }
         seedData(newData)
         adapterReference.get()?.let {
-            diffResult.dispatchUpdatesTo(it)
+            result.dispatchUpdatesTo(it)
         }
     }
 
--- common/src/main/java/com/netchar/common/poweradapter/adapter/RecyclerDiffCallback.kt
+++ common/src/main/java/com/netchar/common/poweradapter/adapter/RecyclerDiffCallback.kt
@@ -1,12 +1,33 @@
+/*
+ * Copyright © 2019 Eugene Glushankov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.netchar.common.poweradapter.adapter
 
 import androidx.recyclerview.widget.DiffUtil
 import com.netchar.common.poweradapter.item.IRecyclerItem
 
-class RecyclerDiffCallback internal constructor(
-    private val oldList: List<IRecyclerItem>,
-    private val newList: List<IRecyclerItem>
-) : DiffUtil.Callback() {
+class RecyclerDiffCallback : DiffUtil.Callback() {
+
+    private lateinit var oldList: List<IRecyclerItem>
+    private lateinit var newList: List<IRecyclerItem>
+
+    fun update(oldList: List<IRecyclerItem>, newList: List<IRecyclerItem>) {
+        this.oldList = oldList
+        this.newList = newList
+    }
 
     override fun getOldListSize(): Int {
         return oldList.size
